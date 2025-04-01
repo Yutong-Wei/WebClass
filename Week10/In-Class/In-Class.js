89d8b2ec16c6a4d9fe175cad0b221765ada74ddb
Yutong-Wei <yw6975@nyu.edu>
2025-04-01 09:51:11 -0400

    Week 10_Class 1_in-class

---
+++ Week10/In-Class/In-Class.js
@@ -0,0 +1,27 @@
+// var x = 5 + 2 + 3;
+// var y = 6.7;
+// let carName="Volvo";
+// const z =7;
+// let nah = false;
+
+
+// document.getElementById("car").innerHTML = carName + " " + "was my first car";
+
+// document.getElementById("alertButton").onclick = function(){
+//     window.alert("hi")
+// };
+
+
+// document.getElementById("mathProb").innerHTML = 
+// "the value of x is"  + x + "nah";
+
+let name = "Yutong";
+let age = "20";
+let coding = true;
+
+document.getElementById("practice").innerHTML = 
+"Hello, my name is " + 
+name + " I am " + 
+age + " years old and I am " +
+"and I can code JavaScript: " +
+coding;
---
+++ Week10/In-Class/Week10Note.md
@@ -0,0 +1,14 @@
+# Week 10_Class 1_4/1/2025
+**JavaScript:** no build in, need to run on the actual browser
+
+**Variable**
+"let" = set the variables
+no spaces
+
+semicolon at the end of every sentence
+
+syntax: the rule of JavaScript
+strings: text
+"=": used to assign values to  variable
+
+bool: true or false
